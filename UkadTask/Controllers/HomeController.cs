93b1759d7e064f0af5b1ec9ad3e88db2e5b86c68
Danny Tim <dantin241@gmail.com>
2019-10-30 23:03:15 +0200

    SpeedTest view fixed

--- UkadTask/Controllers/HomeController.cs
+++ UkadTask/Controllers/HomeController.cs
@@ -10,9 +10,29 @@ namespace UkadTask.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult SpeedTest(string sourceUrl)
         {
-            return View();
+            DBService dbs = new DBService();
+            if (string.IsNullOrWhiteSpace(sourceUrl))
+            {
+                //DBService dbs = new DBService();
+                return View("~/Views/View.cshtml", new URLInfoViewModel { URLInfo = dbs.GetAllURLInfos("abcdef"), SourceUrl = sourceUrl });
+            }
+
+            //SiteMapReader smr = new SiteMapReader(sourceUrl);
+            //List<URLInfo> result = smr.MeasureResponseTime();
+
+            List<URLInfo> exampleList = new List<URLInfo>();
+
+            //exampleList.Add(new URLInfo() { Url = "aaa", ElapsedTime = 111 });
+
+            //dbs.CreateTableForURLs(sourceUrl);
+            //dbs.AddURLInfos(exampleList, sourceUrl);
+            //dbs.GetURLInfos(sourceUrl);
+            //dbs.GetByNameURLInfos(sourceUrl, "c");
+
+            //return View("~/Views/Shared/Error.cshtml");
+            return View("~/Views/View.cshtml", new URLInfoViewModel { URLInfo = dbs.GetAllURLInfos(sourceUrl), SourceUrl = sourceUrl });
         }
 
         public ActionResult About()
